64c7fe8ba1b9e3740bd5f117b45b1f9c05be6a2b
Tyrol <tyrol@gryphon.zone>
2019-06-25 00:50:06 -0500

    WIP

--- vars/dockerImagePipeline.groovy
+++ vars/dockerImagePipeline.groovy
@@ -152,8 +152,8 @@ def call(String githubOrganization, Closure body) {
                                 String propertiesToString = String.join("\n", calculatedJobProperties.collect { prop -> "\t${prop}".replace('<anonymous>=', '') })
 
                                 echo """\
-                                    ${'#' * 140}
-                                    ${'#' * 140}
+                                    ${'#' * 120}
+                                    ${'#' * 120}
                                     Calculated Configuration:
                                     -------------------------
                                     Github Organization: ${githubOrganization}
@@ -164,8 +164,8 @@ def call(String githubOrganization, Closure body) {
                                     Docker build context: ${buildContext}
                                     """.stripIndent()
                                         .concat("Job Properties:\n${propertiesToString}\n")
-                                        .concat('#' * 140).concat('\n')
-                                        .concat('#' * 140).concat('\n')
+                                        .concat('#' * 120).concat('\n')
+                                        .concat('#' * 120).concat('\n')
 
                                 String buildTag = dockerUtilities.coordinatesFor(dockerOrganization, artifact, Util.entropy())
 
