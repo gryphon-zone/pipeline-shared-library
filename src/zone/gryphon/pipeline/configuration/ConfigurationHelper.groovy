4a7193ca414ab20ab9cff1485718dd188a5fe311
Tyrol <tyrol@gryphon.zone>
2019-07-10 23:54:40 -0500

    wip

--- src/zone/gryphon/pipeline/configuration/ConfigurationHelper.groovy
+++ src/zone/gryphon/pipeline/configuration/ConfigurationHelper.groovy
@@ -79,7 +79,7 @@ void printConfiguration(Map effectiveConfiguration) {
         int paddingLength = maxKeyLength - key.length()
         String padding = paddingLength > 0 ? (' ' * paddingLength) : ''
 
-        configurationMessage += c.green(key + padding + ':') + c.blue(value) + '\n'
+        configurationMessage += c.green(key + padding + ':') + ' ' + c.blue(value) + '\n'
     }
 
     for (String key : sortedMultiLineKeys) {
