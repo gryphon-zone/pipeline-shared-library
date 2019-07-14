64b586a4ecdbf03397962622bf5c4c550ca05861
Tyrol <tyrol@gryphon.zone>
2019-07-14 01:39:22 -0500

    Add logging library

---
+++ vars/log.groovy
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019-2019 Gryphon Zone
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import zone.gryphon.pipeline.toolbox.TextColor
+
+private boolean isColorSupported() {
+    // ansi color plugin sets the TERM environment variable when color is enabled
+    return env.TERM != null
+}
+
+enum Level {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
+}
+
+private void logMessage(Level level, String message) {
+    TextColor color = TextColor.instance
+    String basePrefix = "[${level.name()}]"
+    String prefix
+
+    if (isColorSupported()) {
+        switch (level) {
+            case Level.INFO:
+                prefix = color.blue(basePrefix)
+                break
+            case Level.WARN:
+                prefix = color.yellow(basePrefix)
+                break
+            case Level.ERROR:
+                prefix = color.red(basePrefix)
+                break
+
+            default:
+                prefix = basePrefix
+        }
+    } else {
+        prefix = basePrefix
+    }
+
+    echo("${prefix} ${message}")
+}
+
+void info(String message, Object... parts) {
+    logMessage(Level.INFO, String.format(message, parts))
+}
+
+void warn(String message, Object... parts) {
+    logMessage(Level.WARN, String.format(message, parts))
+}
+
+void error(String message, Object... parts) {
+    logMessage(Level.ERROR, String.format(message, parts))
+}
