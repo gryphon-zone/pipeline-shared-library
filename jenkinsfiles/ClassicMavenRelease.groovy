0bb15076243c2652b9d319eb440d095ca260f7ee
Tyrol <tyrol@gryphon.zone>
2019-11-07 22:43:46 -0600

    wip

--- jenkinsfiles/ClassicMavenRelease.groovy
+++ jenkinsfiles/ClassicMavenRelease.groovy
@@ -1,3 +1,4 @@
+#!/usr/bin/env groovy
 /*
  * Copyright 2019-2019 Gryphon Zone
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,14 +15,65 @@
  */
 @Library('gryphon-zone/pipeline-shared-library@master') _
 
+import zone.gryphon.pipeline.toolbox.ScopeUtility
+import zone.gryphon.pipeline.toolbox.Util
+
+void main() {
+    final ScopeUtility scope = new ScopeUtility()
+    final Util util = new Util()
+
+    String organization = params['Github Organization']
+    String repo = params['Repository']
+    String branch = params['Branch']
+
+    String release =  params['Release Version']
+    String postRelease =  params['Post Release Version']
+
+    String jdk = params['JDK version']
+
+
+    // run build inside of docker build image
+    scope.inDockerImage("maven:3-jdk-${jdk}", args: '-v jenkins-shared-m2-cache:/root/.m2/repository') {
+        sshagent(['github-ssh']) {
+            util.sh('mkdir -p ~/.ssh && echo StrictHostKeyChecking no >> ~/.ssh/config', quiet: true)
+
+            git(credentialsId: 'github-ssh', url: "git@github.com:${organization}/${repo}.git", branch: branch)
+
+            echo "release: ${release}, post release: ${postRelease}"
+        }
+    }
+}
+
 //noinspection GroovyAssignabilityCheck
 properties([
         parameters([
                 choice(name: 'Github Organization', choices: ['gryphon-zone', 'chief-tyrol'], description: ''),
-                string(name: 'Repository', defaultValue: '', description: '', trim: false),
+                string(name: 'Repository', defaultValue: '', description: '', trim: true),
+                string(name: 'Branch', defaultValue: 'master', description: '', trim: true),
                 string(name: 'Release Version', defaultValue: '', description: '', trim: true),
                 string(name: 'Post Release Version', defaultValue: '', description: '', trim: true),
                 choice(name: 'JDK version', choices: ['11', '8', '9', '10', '12', '13', '14'], description: '')
 
         ])
 ])
+
+final ScopeUtility scope = new ScopeUtility()
+
+// add support for ANSI color
+scope.withColor {
+
+    // add timestamps to build logs
+    scope.withTimestamps {
+
+        // no build is allowed to run for more than 1 hour
+        scope.withAbsoluteTimeout(60) {
+
+            // allocate executor node
+            scope.withExecutor('docker') {
+
+                main()
+
+            }
+        }
+    }
+}
