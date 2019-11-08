681327d5e7ee9b24aef0a27fa925d975d47500ca
Tyrol <tyrol@gryphon.zone>
2019-11-07 23:59:46 -0600

    WIP

--- jenkinsfiles/ClassicMavenRelease.groovy
+++ jenkinsfiles/ClassicMavenRelease.groovy
@@ -15,56 +15,112 @@
  */
 @Library('gryphon-zone/pipeline-shared-library@master') _
 
-import zone.gryphon.pipeline.toolbox.ScopeUtility
 import zone.gryphon.pipeline.toolbox.Util
-
+import zone.gryphon.pipeline.toolbox.ScopeUtility
 import groovy.transform.Field
 
-//@Field final String
+@Field final String PARAM_ORG = 'Github Organization'
+@Field final String PARAM_REPO = 'Repository'
+@Field final String PARAM_BRANCH = 'Branch'
+@Field final String PARAM_RELEASE_VERSION = 'Release Version'
+@Field final String PARAM_NEXT_VERSION = 'Post Release Version'
+@Field final String PARAM_JDK_VERSION = 'JDK version'
+
+@Field final List<String> WHITELISTED_ORGS = ['gryphon-zone', 'chief-tyrol']
+
+boolean isBlank(String input) {
+    return input == null || input == '' || input.trim() == ''
+}
 
 void main() {
     final ScopeUtility scope = new ScopeUtility()
     final Util util = new Util()
+    final String mvn = 'mvn -B -V -Dstyle.color=always'
 
-    String organization = params['Github Organization']
-    String repo = params['Repository']
-    String branch = params['Branch']
+    String releaseVersionArgument
+    String postReleaseVersionArgument
 
-    String release =  params['Release Version']
-    String postRelease =  params['Post Release Version']
+    String organization = params[PARAM_ORG]
+    String repo = params[PARAM_REPO]
+    String branch = params[PARAM_BRANCH]
 
-    String jdk = params['JDK version']
+    String releaseVersion = params[PARAM_RELEASE_VERSION]
+    String postReleaseVersion = params[PARAM_NEXT_VERSION]
 
+    String jdk = params[PARAM_JDK_VERSION]
+
+    if (!WHITELISTED_ORGS.contains(organization)) {
+        error("Unable to release from organization \"${organization}\"")
+        return
+    }
+
+    if (isBlank(repo)) {
+        error("Github repository must be specified")
+        return
+    }
+
+    if (isBlank(branch)) {
+        error("Branch must be specified")
+        return
+    }
+
+    if (isBlank(jdk)) {
+        error("JDK version must be specified")
+        return
+    }
+
+    try {
+        jdk.toInteger()
+    } catch (Exception e) {
+        error("Invalid JDK version, must be an integer: \"${jdk}\"")
+        throw e
+    }
+
+    if (isBlank(releaseVersion)) {
+        releaseVersionArgument = ''
+    } else if (releaseVersion.contains("'")) {
+        error("Invalid release version, contains invalid characters: \"${releaseVersion}")
+        return
+    } else {
+        releaseVersionArgument = "-DreleaseVersion='${releaseVersion}'"
+    }
+
+    if (isBlank(postReleaseVersion)) {
+        postReleaseVersionArgument = ''
+    } else if (postReleaseVersion.contains("'")) {
+        error("Invalid post-release version, contains invalid characters: \"${postReleaseVersion}")
+        return
+    } else if (!postReleaseVersion.endsWith('-SNAPSHOT')) {
+        error("Invalid post-release version, must end in \"-SNAPSHOT\": \"${postReleaseVersion}")
+        return
+    } else {
+        postReleaseVersionArgument = "-DdevelopmentVersion='${postReleaseVersion}'"
+    }
 
     // run build inside of docker build image
     scope.inDockerImage("maven:3-jdk-${jdk}", args: '-v jenkins-shared-m2-cache:/root/.m2/repository') {
         sshagent(['github-ssh']) {
-            util.sh('mkdir -p ~/.ssh && echo StrictHostKeyChecking no >> ~/.ssh/config', quiet: true)
-
-            util.installMavenSettingsFile()
-
-
-            git(credentialsId: 'github-ssh', url: "git@github.com:${organization}/${repo}.git", branch: branch)
-
-            util.enableGitColor()
-
-            util.sh("""\
-            git config user.email 'jenkins@gryphon.zone' && \
-            git config user.name 'Jenkins' \
-            """.stripIndent().trim(), returnType: 'none')
-
             scope.withGpgKey('gpg-signing-key-id', 'gpg-signing-key', 'GPG_KEYID') {
                 withCredentials([usernamePassword(credentialsId: 'ossrh', usernameVariable: 'OSSRH_USERNAME', passwordVariable: 'OSSRH_PASSWORD')]) {
 
+                    util.sh('mkdir -p ~/.ssh && echo StrictHostKeyChecking no >> ~/.ssh/config', quiet: true)
+
+                    util.installMavenSettingsFile()
+
+                    git(credentialsId: 'github-ssh', url: "git@github.com:${organization}/${repo}.git", branch: branch)
+
+                    util.sh('''
+                    git config user.email 'jenkins@gryphon.zone'
+                    git config user.name 'Jenkins'
+                    ''', returnType: 'none')
+
                     util.sh("""
                     export MAVEN_OPTS='-Djansi.force=true'
-                    mvn release:prepare -B -V -Dstyle.color=always -DreleaseVersion='${release}' -DdevelopmentVersion='${postRelease}'
-                    mvn release:perform -B -V -Dstyle.color=always -Dossrh.username='${OSSRH_USERNAME}' -Dossrh.password='${OSSRH_PASSWORD}'
+                    ${mvn} release:prepare ${releaseVersionArgument} ${postReleaseVersionArgument} -Darguments='-Dstyle.color=always'
+                    ${mvn} release:perform -Dossrh.username='${OSSRH_USERNAME}' -Dossrh.password='${OSSRH_PASSWORD}' -Darguments='-Dstyle.color=always -DskipTests=true'
                     """, returnType: 'none')
                 }
             }
-
-            echo "release: ${release}, post release: ${postRelease}"
         }
     }
 }
@@ -72,12 +128,12 @@ void main() {
 //noinspection GroovyAssignabilityCheck
 properties([
         parameters([
-                choice(name: 'Github Organization', choices: ['gryphon-zone', 'chief-tyrol'], description: ''),
-                string(name: 'Repository', defaultValue: '', description: '', trim: true),
-                string(name: 'Branch', defaultValue: 'master', description: '', trim: true),
-                string(name: 'Release Version', defaultValue: '', description: '', trim: true),
-                string(name: 'Post Release Version', defaultValue: '', description: '', trim: true),
-                choice(name: 'JDK version', choices: ['11', '8', '9', '10', '12', '13', '14'], description: '')
+                choice(name: PARAM_ORG, choices: WHITELISTED_ORGS, description: ''),
+                string(name: PARAM_REPO, defaultValue: '', description: '', trim: true),
+                string(name: PARAM_BRANCH, defaultValue: 'master', description: '', trim: true),
+                string(name: PARAM_RELEASE_VERSION, defaultValue: '', description: '', trim: true),
+                string(name: PARAM_NEXT_VERSION, defaultValue: '', description: '', trim: true),
+                choice(name: PARAM_JDK_VERSION, choices: ['11', '8', '9', '10', '12', '13', '14'], description: '')
 
         ])
 ])
